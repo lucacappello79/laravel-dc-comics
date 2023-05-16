d2a68e0354cd3429c48d655bda979900aa8c952a
lucacappello79 <postt79@gmail.com>
2023-05-16 16:27:04 +0200

    fix: changing fillable

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'description', 'type', 'image', 'cooking_time', 'weight', 'src'];
+    protected $fillable = ['title', 'description', 'type', 'series', 'price'];
 }
