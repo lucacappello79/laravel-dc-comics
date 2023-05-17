600caa54f8be6cb710283ea6239bcbd16aa457a2
lucacappello79 <postt79@gmail.com>
2023-05-17 17:41:18 +0200

    fix: updating routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\ComicController;
-use App\Http\Controllers\Guest\PageController;
+// use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PageController::class, 'index'])->name('homepage');
+// Route::get('/', [PageController::class, 'index'])->name('homepage');
+Route::get('/', [ComicController::class, 'index'])->name('homepage');
 Route::resource('/comics', ComicController::class);
