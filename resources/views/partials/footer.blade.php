619cd7dc8719267d1441a7d2dfe5bef2f1880199
lucacappello79 <postt79@gmail.com>
2023-05-19 15:04:51 +0200

    fix: added a couple of comments

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,3 +3,35 @@
    Foooter
   </div>
 </footer>
+
+{{-- <div class="footer-wrapper">
+  <div class="bottom-links">
+      <div class="col-left">
+          <ul v-for="(singleList, singleListIndex) in footerLinks.slice(0, 2)">
+              <li>
+                  <strong>{{ singleList.title }}</strong>
+                  <ul>
+                      <li v-for="(item, itemIndex) in singleList.lista">
+                          {{ item }}
+                      </li>
+                  </ul>
+              </li>
+          </ul>
+      </div>
+      <div class="col-right">
+          <ul v-for="(singleList, singleListIndex) in footerLinks.slice(2)">
+              <li>
+                  <strong>{{ singleList.title }}</strong>
+                  <ul>
+                      <li v-for="(item, itemIndex) in singleList.lista">
+                          {{ item }}
+                      </li>
+                  </ul>
+              </li>
+          </ul>
+      </div>
+  </div>
+  <div class="footer-img-wrapper">
+      <img src="/img/dc-logo-bg.png" alt="">
+  </div>
+</div> --}}
